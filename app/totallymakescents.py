d3da17ba8448f6e03018573f84007fdf9640376b
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-04-17 17:42:31 -0400

    Update app

--- app/totallymakescents.py
+++ app/totallymakescents.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import sys 
-sys.path.append('../recommender/version_gamma/')
+import pathlib 
+sys.path.append(sys.path.append(str(pathlib.Path(__file__))) + '../recommender/version_gamma/')
 
 path_total = 'https://raw.githubusercontent.com/juntongsu/totallymakescents/refs/heads/main/'
 path_app = path_total + 'app/'
