99c782c9643a599367d1fa47c32f25f021cd6f8d
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-04-17 18:56:31 -0400

    Update version gamma

--- recommender/version_gamma/recommender_gamma.py
+++ recommender/version_gamma/recommender_gamma.py
@@ -4,20 +4,22 @@ path_data = home + 'data/'
 from recommender_users_func import *
 from recommender_notes_func import *
 
-df_persian = pd.read_csv(path_data + 'Persian_Data.csv')
-df_persian_new = df_persian[['User ID', 'Perfume Name','Perfume ID', 'Sentiment']]
-df_persian_new = df_persian_new.dropna()
+# df_persian = pd.read_csv(path_data + 'Persian_Data.csv')
+# df_persian_new = df_persian[['User ID', 'Perfume Name','Perfume ID', 'Sentiment']]
+# df_persian_new = df_persian_new.dropna()
 
-##Remove perfumes with same name and different ids
-black_list = {15195,9500,15615}
-df_persian_new = df_persian_new.loc[(~df_persian_new["Perfume ID"].isin(black_list))][['User ID','Perfume Name','Sentiment']]
+# ##Remove perfumes with same name and different ids
+# black_list = {15195,9500,15615}
+# df_persian_new = df_persian_new.loc[(~df_persian_new["Perfume ID"].isin(black_list))][['User ID','Perfume Name','Sentiment']]
 
-user_ids = df_persian_new['User ID'].unique()
-user_frame = pd.DataFrame({'User ID':user_ids})
-user_samp = user_frame.sample(frac=0.80, random_state=13)
-selection = user_samp["User ID"]
-persian_data_frame_clean = df_persian_new.loc[(df_persian_new['User ID'].isin(selection) )]
-persian_data_frame_clean.update(persian_data_frame_clean["Sentiment"].apply(enum_ratings))
+# user_ids = df_persian_new['User ID'].unique()
+# user_frame = pd.DataFrame({'User ID':user_ids})
+# user_samp = user_frame.sample(frac=0.80, random_state=13)
+# selection = user_samp["User ID"]
+# persian_data_frame_clean = df_persian_new.loc[(df_persian_new['User ID'].isin(selection) )]
+# persian_data_frame_clean.update(persian_data_frame_clean["Sentiment"].apply(enum_ratings))
+
+persian_data_frame_clean = pd.read_csv('{}cleaned_persian.csv'.format(path_data))
 
 user_frame = pd.read_csv('input.csv', header=None)
 user_frame.columns = ["Perfume Name","Sentiment"]
