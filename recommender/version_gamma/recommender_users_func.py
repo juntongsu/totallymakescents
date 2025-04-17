c65f07b2aca2d2bc64b93a4ec4d90d878a5f8611
jtarasov <ytarasov@umich.edu>
2025-04-17 18:47:01 -0400

    Update recommender_users_func.py

--- recommender/version_gamma/recommender_users_func.py
+++ recommender/version_gamma/recommender_users_func.py
@@ -44,30 +44,34 @@ def cos_index_1(user_1,user_2,frame_1,frame_2):
     vector_2 = sentiment_vector(user_2, frame_2, shared)
     return cosine_similarity([vector_1],[vector_2])[0][0]
 
-def adjust_rating_1(rating,user_1,user_2,frame_1,frame_2,weight=1.0):
-    return rating*cos_index_1(user_1,user_2,frame_1,frame_2)*accuracy_index(user_1, user_2, frame_1,frame_2,weight)
+def multiplier(user_1,user_2,frame_1,frame_2,weight=1.0):
+    return cos_index_1(user_1,user_2,frame_1,frame_2)*accuracy_index(user_1, user_2, frame_1,frame_2,weight)
+
+def adjust_rating_1(rating, multi):
+    return rating*multi
 
 def adjusted_frame_1(user_1,frame_1,frame_2, commonality=1,weight = 1.0):
     relevant_users = reference_users(user_1, frame_1,frame_2, commonality)
     new_frame = frame_2.loc[(frame_2['User ID'].isin(relevant_users))]
     for user_2 in relevant_users:
-        adjust = new_frame.loc[(new_frame['User ID']==user_2), "Sentiment"].apply(adjust_rating_1,args=(user_1,user_2,frame_1,frame_2,weight))
+        m = multiplier(user_1,user_2,frame_1,frame_2,weight)
+        adjust = new_frame.loc[(new_frame['User ID']==user_2), "Sentiment"].apply(adjust_rating_1,args=([m]))
         new_frame.update(adjust)
     return new_frame
 
-def ratings(user_1,frame_1,frame_2, commonality=1,weight=1.0):
+def ratings(user_1,frame_1,frame_2, commonality=1,weight=0, type = "mean"):
     known_perfumes = has_opinions(user_1, frame_1)
 
     adjusted_ratings = adjusted_frame_1(user_1,frame_1,frame_2,commonality,weight)
     adjusted_ratings = adjusted_ratings.loc[(~adjusted_ratings["Perfume Name"].isin(known_perfumes))]
 
-    adjusted_ratings["Similarity"] = adjusted_ratings.groupby('Perfume Name')['Sentiment'].transform('sum')
+    adjusted_ratings["Similarity"] = adjusted_ratings.groupby('Perfume Name')['Sentiment'].transform(type)
     adjusted_ratings = adjusted_ratings[["Perfume Name", "Similarity"]].drop_duplicates()
 
     return adjusted_ratings
 
-def recommender_users(user_1,frame_1,frame_2, commonality=2,weight=1, top=20):
-    adjusted_ratings = ratings(user_1,frame_1,frame_2, commonality, weight)
+def recommender_users(user_1,frame_1,frame_2 = persian_data_frame_clean, commonality=2,weight=0, top=20, type="mean"):
+    adjusted_ratings = ratings(user_1,frame_1,frame_2, commonality, weight, type)
     rec_list = adjusted_ratings.convert_dtypes().nlargest(top, 'Similarity', keep = "all")
     
     return rec_list[["Perfume Name", "Similarity"]]
