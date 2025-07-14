192aba2250534724bfcc422b2cec679ee1c26844
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-07-14 15:43:21 -0400

    update app

--- app/pages/tms_potd.py
+++ app/pages/tms_potd.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import sys 
 import pathlib
-import time
+import datetime
 sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent.parent))
 # path_total = '../'
 
@@ -18,14 +18,36 @@ st.title('Perfume of the Day')
 
 df_fra_standard = pd.read_csv('{}fra_standard.csv'.format(path_data))
 
-potd = df_fra_standard.sample(1)
+st.write(':material/calendar_today: ', datetime.date.today())
 
-# potd
+today = datetime.date.toordinal(datetime.date.today())
+index_potd = df_fra_standard.sample(1, random_state=today).index[0]
+potd = df_fra_standard.iloc[index_potd]
 
-st.subheader('{}'.format(potd['Perfume'].values))
-st.text('Brand: {}'.format(potd['Brand'].values))
-st.text('Top Notes: {}'.format(potd['Top'].values))
-st.text('Middle Notes: {}'.format(potd['Middle'].values))
-st.text('Base Notes: {}'.format(potd['Base'].values))
-st.text('Main Accords: {} {} {}'.format(potd['mainaccord1'].values, potd['mainaccord2'].values, potd['mainaccord3'].values))
-# st.link_button('Info and Reviews', '{}'.format(potd['url'].values))
+st.subheader(':material/fragrance: {}'.format(potd['Perfume']))
+# st.write(':material/branding_watermark: Brand: {}'.format(potd['Brand']))
+
+col1, col2 = st.columns([1,2])
+with col1:
+    st.write(':material/branding_watermark: Brand')
+    st.write(':material/ent: Main Accords')
+    st.write(':material/clock_loader_10: Top Notes')
+    st.write(':material/clock_loader_40: Middle Notes')
+    st.write(':material/clock_loader_90: Base Notes')
+    
+with col2:
+    st.write('{}'.format(potd['Brand']))
+    st.write('{}, {}, {}, {}, and {}'.format(potd['mainaccord1'], 
+                                            potd['mainaccord2'], 
+                                            potd['mainaccord3'], 
+                                            potd['mainaccord4'], 
+                                            potd['mainaccord5']))
+    st.write('{}'.format(potd['Top']))
+    st.write('{}'.format(potd['Middle']))
+    st.write('{}'.format(potd['Base']))
+
+# st.write('Top Notes: {}'.format(potd['Top']))
+# st.write('Middle Notes: {}'.format(potd['Middle']))
+# st.write('Base Notes: {}'.format(potd['Base']))
+# st.write('Main Accords: {}, {}, {}, {}, and {}'.format(potd['mainaccord1'], potd['mainaccord2'], potd['mainaccord3'], potd['mainaccord4'], potd['mainaccord5']))
+st.link_button('Details and Reviews on Fragrantica.com :material/arrow_outward: ', '{}'.format(potd['url']))
--- app/totallymakescents.py
+++ app/totallymakescents.py
@@ -3,14 +3,14 @@ import streamlit as st
 pages = {
     'Find Scents': [
         st.Page('pages/tms1.py', title = 'TotallyMakeScents', icon = ':material/search:'),
-        st.Page('pages/tms_newbee.py', title = 'Totally New'),
-        st.Page('pages/tms_potd.py', title = 'POTD'),
+        st.Page('pages/tms_newbee.py', title = 'Totally New', icon = ':material/psychology_alt:'),
+        st.Page('pages/tms_potd.py', title = 'POTD', icon = ':material/calendar_today:'),
     ],
     'About': [
-        st.Page('pages/about_us.py', title = 'Meet the Team'),
-        st.Page('pages/help.py', title = 'Help'),
+        st.Page('pages/about_us.py', title = 'Meet the Team', icon = ':material/person:'),
+        st.Page('pages/help.py', title = 'Help', icon = ':material/help:'),
     ]
 }
 
-pg = st.navigation(pages, position='top')
+pg = st.navigation(pages)
 pg.run()
