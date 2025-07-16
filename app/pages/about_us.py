d35a1c7331c9f243ffd4ed1d507b889f6e033be8
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-07-15 20:18:04 -0400

    update app

--- app/pages/about_us.py
+++ app/pages/about_us.py
@@ -1,29 +1,42 @@
 import streamlit as st 
 
-img_path = 'https://raw.githubusercontent.com/juntongsu/totallymakescents/refs/heads/main/app/images/'
+path_total = 'https://raw.githubusercontent.com/juntongsu/totallymakescents/refs/heads/main/'
+img_path = path_total+'app/images/'
 
-st.title('About Us')
+st.title('Meet the Team')
 
-st.subheader('Elly')
-img1,txt1 = st.columns(2)
+img1, txt1, search1 = st.columns(3)
 img1.image(img_path+'elly.png')
-txt1.write('(https://github.com/ellydo17)')
+txt1.subheader('Elly')
+txt1.markdown("[![Repo](https://badgen.net/badge/github/Github/purple?icon&label)](https://github.com/ellydo17)", unsafe_allow_html=True)
 txt1.write('I like to travel!')
+with search1:
+    with st.expander("Elly's Favorite Search"):
+        st.link_button('A magical scent :magic_wand: in a mystical forest :evergreen_tree: with herbs :herb: and secrets:crystal_ball:.', '')
 
-st.subheader('Fernando')
-img2,txt2 = st.columns(2)
+img2, txt2 = st.columns([1, 2])
 img2.image(img_path+'fernando.jpg')
-txt2.write('(https://github.com/fernando-liu-lopez)')
+txt2.subheader('Fernando')
+txt2.markdown("[![Repo](https://badgen.net/badge/github/Github/green?icon&label)](https://github.com/fernando-liu-lopez)", unsafe_allow_html=True)
 txt2.write('[fill]')
 
-st.subheader('Katherine')
-img3,txt3 = st.columns(2)
+img5, txt5 = st.columns([1, 2])
+img5.image(img_path+'su.png')
+txt5.subheader('Jonah')
+txt5.markdown("[![Repo](https://badgen.net/badge/github/Github/red?icon&label)](https://github.com/jtarasov)", unsafe_allow_html=True)
+txt5.write('[fill]')
+
+img3, txt3 = st.columns([1, 2])
 img3.image(img_path+'katherine.jpg')
-txt3.write('(https://github.com/kmerkl22)')
+txt3.subheader('Katherine')
+txt3.markdown("[![Repo](https://badgen.net/badge/github/Github/blue?icon&label)](https://github.com/kmerkl22)", unsafe_allow_html=True)
 txt3.write('I enjoy reading and painting.')
 
-st.subheader('Su')
-img4,txt4 = st.columns(2)
+img4, txt4, search4 = st.columns(3)
 img4.image(img_path+'su.jpg')
-txt4.write('(https://juntongsu.github.io/#)')
-txt4.write('Panda, an animal, eats shoots and leaves')
+txt4.subheader('Su')
+txt4.markdown("[![Repo](https://badgen.net/badge/github/Github/pink?icon&label)](https://github.com/juntongsu)", unsafe_allow_html=True)
+txt4.write('Panda, an animal, eats shoots and leaves.')
+with search4:
+    with st.expander("Su's Favorite Search"):
+        st.link_button('A bittersweet scent :performing_arts: for a farewell party :balloon:.', '')
--- app/pages/tms_potd.py
+++ app/pages/tms_potd.py
@@ -25,9 +25,8 @@ index_potd = df_fra_standard.sample(1, random_state=today).index[0]
 potd = df_fra_standard.iloc[index_potd]
 
 st.subheader(':material/fragrance: {}'.format(potd['Perfume']))
-# st.write(':material/branding_watermark: Brand: {}'.format(potd['Brand']))
 
-col1, col2 = st.columns([1,2])
+col1, col2 = st.columns([1, 2])
 with col1:
     st.write(':material/branding_watermark: Brand')
     st.write(':material/ent: Main Accords')
@@ -46,8 +45,4 @@ with col2:
     st.write('{}'.format(potd['Middle']))
     st.write('{}'.format(potd['Base']))
 
-# st.write('Top Notes: {}'.format(potd['Top']))
-# st.write('Middle Notes: {}'.format(potd['Middle']))
-# st.write('Base Notes: {}'.format(potd['Base']))
-# st.write('Main Accords: {}, {}, {}, {}, and {}'.format(potd['mainaccord1'], potd['mainaccord2'], potd['mainaccord3'], potd['mainaccord4'], potd['mainaccord5']))
 st.link_button('Details and Reviews on Fragrantica.com :material/arrow_outward: ', '{}'.format(potd['url']))
