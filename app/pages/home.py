9999cba1f44096d60b2a47717aa17b2fcb467d19
fernando-liu-lopez <fllfernando95@gmail.com>
2025-08-06 16:08:27 -0500

    separated web scraping from generated training data

--- app/pages/home.py
+++ app/pages/home.py
@@ -18,7 +18,6 @@ import torch
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-
 #from webdriver_manager.chrome import ChromeDriverManager
 #from selenium.webdriver.chrome.service import Service as ChromeService
 #from webdriver_manager.firefox import GeckoDriverManager
