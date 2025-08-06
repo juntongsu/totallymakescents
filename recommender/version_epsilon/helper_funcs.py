ac5960156ba32b1fed596948f622ee12149d4cc6
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-08-05 23:11:35 -0400

    deploy app

--- recommender/version_epsilon/helper_funcs.py
+++ recommender/version_epsilon/helper_funcs.py
@@ -22,7 +22,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver            
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service# as ChromeService
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -35,12 +35,13 @@ from selenium_stealth import stealth
 @st.cache_data(show_spinner=False)
 def scrape_perfume(website):
     # Visit specific perfume website and obtain html code
-    service = ChromeService(ChromeDriverManager().install())
+    # service = ChromeService(ChromeDriverManager().install())
 
     opts = Options()
     opts.add_argument("--headless")
     opts.add_argument("--disable-gpu")
-    service = ChromeService(ChromeDriverManager().install())
+    # service = ChromeService(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=opts)
     stealth(
         driver,
