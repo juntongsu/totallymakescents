b20e41cb60902a9add636cf691bb4992920c1a2d
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-08-09 21:39:55 -0400

    update app

--- app/pages/test.py
+++ app/pages/test.py
@@ -7,14 +7,42 @@ import streamlit as st
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
-
+from webdriver_manager.core.os_manager import ChromeType
+import subprocess
+import shutil
 import pandas as pd
+
+def get_chromium_version() -> str:
+    try:
+        result = subprocess.run(['chromium', '--version'], capture_output=True, text=True)
+        version = result.stdout.split()[1]
+        return version
+    except Exception as e:
+        return str(e)
+
+def get_chromedriver_version() -> str:
+    try:
+        result = subprocess.run(['chromedriver', '--version'], capture_output=True, text=True)
+        version = result.stdout.split()[1]
+        return version
+    except Exception as e:
+        return str(e)
+
+def get_chromedriver_path() -> str:
+    return shutil.which('chromedriver')
+
+st.text('This is only for debugging purposes.\n'
+        'Checking versions installed in environment:\n\n'
+        f'- Chromedriver:  {get_chromedriver_version()}\n'
+        f'- Chromium:      {get_chromium_version()}\n'
+        f'- Chromedriver Path: {get_chromedriver_path()}'
+        )
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--window-size=1920x1080')
 chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument("--browserVersion=114")
+# chrome_options.add_argument("--browserVersion=114")
 # 114.0.5735.90
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
@@ -32,3 +60,10 @@ driver.quit()
 
 df = pd.DataFrame(data, columns=['Quotes'])
 st.dataframe(df)
+
+st.text('This is only for debugging purposes.\n'
+        'Checking versions installed in environment:\n\n'
+        f'- Chromedriver:  {get_chromedriver_version()}\n'
+        f'- Chromium:      {get_chromium_version()}\n'
+        f'- Chromedriver Path: {get_chromedriver_path()}'
+        )
