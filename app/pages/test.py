1d73ea409c2238dbf1a334e38368b6f3ee0c34c8
Juntong Su <44006115+juntongsu@users.noreply.github.com>
2025-08-09 21:57:51 -0400

    update app

--- app/pages/test.py
+++ app/pages/test.py
@@ -1,69 +1,59 @@
-"""Scrapes quotes from the given url.
-
-The url is 'https://quotes.toscrape.com/'.
-"""
-
 import streamlit as st
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.core.os_manager import ChromeType
-import subprocess
-import shutil
-import pandas as pd
+from selenium.webdriver.chrome.service import Service
+import time
+from bs4 import BeautifulSoup
 
-def get_chromium_version() -> str:
+# ------------- Settings for Pages -----------
+st.set_page_config(layout="wide")
+
+# Keep text only
+def get_website_content(url):
+    driver = None
     try:
-        result = subprocess.run(['chromium', '--version'], capture_output=True, text=True)
-        version = result.stdout.split()[1]
-        return version
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+        time.sleep(5)
+        html_doc = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup.get_text()
     except Exception as e:
-        return str(e)
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
 
-def get_chromedriver_version() -> str:
-    try:
-        result = subprocess.run(['chromedriver', '--version'], capture_output=True, text=True)
-        version = result.stdout.split()[1]
-        return version
-    except Exception as e:
-        return str(e)
 
-def get_chromedriver_path() -> str:
-    return shutil.which('chromedriver')
 
-st.text('This is only for debugging purposes.\n'
-        'Checking versions installed in environment:\n\n'
-        f'- Chromedriver:  {get_chromedriver_version()}\n'
-        f'- Chromium:      {get_chromium_version()}\n'
-        f'- Chromedriver Path: {get_chromedriver_path()}'
-        )
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--window-size=1920x1080')
-chrome_options.add_argument('--disable-gpu')
-# chrome_options.add_argument("--browserVersion=114")
-# 114.0.5735.90
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+# ---------------- Page & UI/UX Components ------------------------
+def main_sidebar():
+    # 1.Vertical Menu
+    st.header("Running Selenium on Streamlit Cloud")
+    site_extraction_page()
 
-url = 'https://quotes.toscrape.com/'
 
-driver.get(url)
+def site_extraction_page():
+    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
+    url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
 
-quotes = driver.find_elements('xpath', '//span[@class="text"]')
+    clicked = st.button("Load Page Content",type="primary")
+    if clicked:
+        with st.container(border=True):
+            with st.spinner("Loading page website..."):
+                content = get_website_content(url)
+                st.write(content)
 
-data = []
-for q in quotes:
-    data.append(q.text)
-    
-driver.quit()
 
-df = pd.DataFrame(data, columns=['Quotes'])
-st.dataframe(df)
-
-st.text('This is only for debugging purposes.\n'
-        'Checking versions installed in environment:\n\n'
-        f'- Chromedriver:  {get_chromedriver_version()}\n'
-        f'- Chromium:      {get_chromium_version()}\n'
-        f'- Chromedriver Path: {get_chromedriver_path()}'
-        )
+if __name__ == "__main__":
+    main_sidebar()
